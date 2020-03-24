5caa06b933225c05fae1ec556fd08c003e122472
danny099 <dalmariv01@gmail.com>
2020-03-24 02:57:58 -0500

    connnect with puppet

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+# connect with puppet
+file_line { 'Create_identity_file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
+}
+
+file_line { 'Turn_off_passwd_auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
