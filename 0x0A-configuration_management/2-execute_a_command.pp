fa5608fad39f7fcca5350e6d6f7262aa9c20a3e3
danny099 <dalmariv01@gmail.com>
2020-03-24 02:21:21 -0500

    fix error

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
-# Using Puppet, create a manifest that kills a process named killmenow.
-
-exec { 'pkill -f killmenow':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
+#!/usr/bin/env bash
+#kill procces with puppet
+exec { 'pkill -f ./killmenow':
+    command => 'pkill -f ./killmenow',
+    path    => '/usr/bin',
 }
 
