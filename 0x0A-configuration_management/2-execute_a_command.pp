3e8a7d30a93bac2fb67659316d5248991cdcc6ca
danny099 <dalmariv01@gmail.com>
2020-03-24 01:56:37 -0500

    fix error

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #kill procces with puppet
 exec { 'killmenow':
-  path    => '/usr/bin/',
-  command => 'pkill -f ./killmenow',
+    command => 'pkill -f ./killmenow',
+    path    => '/usr/bin',
 }
