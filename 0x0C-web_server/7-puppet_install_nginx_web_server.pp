cbb16e71d2964e885acd409fccd95bc44e809347
danny099 <dalmariv01@gmail.com>
2020-03-25 23:02:51 -0500

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,1 +1,23 @@
 #!/usr/bin/env bash
+#puppet
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School for the win!',
+}
+
+file_line { 'file':
+  ensure   => present,
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'listen 80 default_server;',
+  line     => 'rewrite ^/redirect_me https://www.linkedin.com/in/danny-alejandro-martinez-rivera-72b470192 permanent;',
+  multiple => true
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
