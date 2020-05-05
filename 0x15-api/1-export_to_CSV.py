b02e7f7ff6e303e589cfa40a6d75e4c8585a63bd
danny099 <dalmariv01@gmail.com>
2020-05-04 23:19:55 -0500

    csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""task 1"""
+import requests
+import csv
+from sys import argv
+
+
+if __name__ == '__main__':
+    id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com'
+    user = requests.get(url+'/users/'+id)
+    name = user.json().get('name')
+    user = requests.get(url+'/todos?userId={}'.format(argv[1]))
+
+    data = user.json()
+
+    with open('{}.csv'.format(argv[1]), mode='w') as f:
+        csv_writer = csv.writer(f, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_ALL)
+        for i in data:
+            csv_writer.writerow(
+                [argv[1], user, i.get('completed'), i.get('title')])
