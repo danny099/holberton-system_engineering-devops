9b954f5928e5e19aaff6d1392dec629f3569ccdb
danny099 <dalmariv01@gmail.com>
2020-05-04 22:47:32 -0500

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""task 0"""
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com'
+    user = requests.get(url+'/users/'+id)
+    name = user.json().get('name')
+    all = requests.get(url+'/todos')
+
+    data = all.json()
+    total = 0
+    completed = 0
+
+    for i in data:
+        total += 1
+        if i.get('completed'):
+            completed += 1
+
+    print("""Employee {} is done with tasks({}/{}):
+        """.format(name, completed, total))
+    for i in data:
+        if i.get('completed'):
+            print('\t {}'.format(i.get('title')))
