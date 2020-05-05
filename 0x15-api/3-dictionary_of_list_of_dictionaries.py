7133f4e20eb9ce99ef1b387f1ef994318e49f385
danny099 <dalmariv01@gmail.com>
2020-05-04 23:55:19 -0500

    dict

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""task 1"""
+
+if __name__ == "__main__":
+
+    import json
+    import requests
+    import sys
+
+    url = "https://jsonplaceholder.typicode.com"
+    users = requests.get(url+"/users")
+    users = users.json()
+    everyBody = requests.get(url+"/todos")
+    everyBody = everyBody.json()
+    obj = {}
+
+    for user in users:
+        tasks = []
+        for i in everyBody:
+            if i.get('userId') == user.get('id'):
+                dic = {"username": user.get('username'),
+                       "task": i.get('title'),
+                       "completed": i.get('completed')}
+                tasks.append(dic)
+        obj[user.get('id')] = tasks
+
+    with open('todo_all_employees.json', mode='w') as f:
+        json.dump(obj, f)
