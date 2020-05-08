556624802e1e306eb0538db97501d3f45d060bd5
danny099 <dalmariv01@gmail.com>
2020-05-07 23:49:03 -0500

    2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+reddit api
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+
+    url = 'https://api.reddit.com/r/{}/hot?after={}'.format(subreddit, after)
+    headers = {'User-Agent': 'danny'}
+    r = requests.get(url, headers=headers).json()
+    try:
+        for i in r.get('data')['children']:
+            hot_list.append(i['data']['title'])
+        if r['data']['after'] is not None:
+            recurse(subreddit, hot_list, r['data']['after'])
+        return hot_list
+    except Exception:
+        return None
