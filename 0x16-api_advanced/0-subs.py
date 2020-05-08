b4507cb98136292d3a89bc723a1563a9a10773e1
danny099 <dalmariv01@gmail.com>
2020-05-07 22:58:48 -0500

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+reddit api
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    if type(subreddit) is not str:
+        return 0
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'user-agent': 'chrome:happy/0.1.0'}
+    data = requests.get(url, headers=headers)
+    if data.status_code != 200:
+        return 0
+    return data.json().get("data").get("subscribers")
